978baf2c72ad50ae09435e2a7959b404665abd45
root <root@10195bafaae9>
2017-05-16 18:06:43 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_KASVWQxEyX",
+  "homePage" : "Main",
+  "name" : "Auto_KASVWQxEyX",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
